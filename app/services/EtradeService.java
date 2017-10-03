a74fca28c323d49e8410e9105580877cc4df19e5
shahid11885 <32446592+shahid11885@users.noreply.github.com>
2017-10-02 22:55:56 -0700

    Initial commit

---
+++ app/services/EtradeService.java
@@ -0,0 +1,17 @@
+package services;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.concurrent.CompletableFuture;
+
+@Singleton
+public class EtradeService {
+
+
+    public String getEtradeData() {
+        // Connect to eTrade and fetch data
+
+        return "";
+
+    }
+}
